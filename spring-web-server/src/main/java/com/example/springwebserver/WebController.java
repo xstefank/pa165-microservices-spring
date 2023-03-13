9fc3d6c92ce8c55cc3ad82f034214692d81afcc8
xstefank <xstefank122@gmail.com>
2023-03-13 14:24:08 +0100

    Add kill endpoint

--- spring-web-server/src/main/java/com/example/springwebserver/WebController.java
+++ spring-web-server/src/main/java/com/example/springwebserver/WebController.java
@@ -1,5 +1,8 @@
 package com.example.springwebserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -11,6 +14,11 @@ import java.util.UUID;
 public class WebController {
 
     private final String id = "Web Server Node " + UUID.randomUUID();
+    private final ApplicationContext context;
+
+    public WebController(ApplicationContext context) {
+        this.context = context;
+    }
 
     @GetMapping(path = "/whereami")
     public Mono<String> whereami() {
@@ -24,5 +32,11 @@ public class WebController {
     @GetMapping(path = "/whoami")
     public String whoami() {
         return "Id: " + id;
+    }
+
+    @GetMapping(path = "/kill")
+    public void kill() {
+        SpringApplication.exit(context, () -> 0);
+        System.exit(0);
     }
 }
